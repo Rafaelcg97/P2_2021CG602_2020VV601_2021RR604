dc8e0dced3d06052c60697f71043eff72ac0dc46
Rafael Castro <castroguzmanrafa@outlook.com>
2023-04-29 08:23:45 -0600

    Cambio en el program para iniciar en el controlador de casosReportados

--- P2_2021CG602_2020VV601_2021RR604/Controllers/casosReportadosController.cs
+++ P2_2021CG602_2020VV601_2021RR604/Controllers/casosReportadosController.cs
@@ -2,7 +2,7 @@
 
 namespace P2_2021CG602_2020VV601_2021RR604.Controllers
 {
-    public class casosReportados : Controller
+    public class casosReportadosController : Controller
     {
         public IActionResult Index()
         {
