9191f8b4bc8ecbb02f72ac3a6eea5c56e51b3624
Rafael Castro <castroguzmanrafa@outlook.com>
2023-04-29 08:04:41 -0600

    controlador de casos reportados

---
+++ P2_2021CG602_2020VV601_2021RR604/Controllers/casosReportados.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P2_2021CG602_2020VV601_2021RR604.Controllers
+{
+    public class casosReportados : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
