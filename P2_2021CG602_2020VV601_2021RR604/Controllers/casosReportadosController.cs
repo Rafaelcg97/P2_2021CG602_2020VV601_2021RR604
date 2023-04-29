b497471a0b35081db8b042704c8f33e1d2cd4b2c
Rafael Castro <castroguzmanrafa@outlook.com>
2023-04-29 08:50:29 -0600

    Error en controller

--- P2_2021CG602_2020VV601_2021RR604/Controllers/casosReportadosController.cs
+++ P2_2021CG602_2020VV601_2021RR604/Controllers/casosReportadosController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using P2_2021CG602_2020VV601_2021RR604.Models;
 
 namespace P2_2021CG602_2020VV601_2021RR604.Controllers
 {
     public class casosReportadosController : Controller
     {
+        private readonly ParcialBDContext _parcialBDContext;
+
+        public ParcialBDContext(ParcialBDContext parcialBDContext)
+        {
+            _parcialBDContext = parcialBDContext;
+        }
         public IActionResult Index()
         {
             return View();
