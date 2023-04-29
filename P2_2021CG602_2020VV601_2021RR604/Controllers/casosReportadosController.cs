ed927f646eee50f26ec2bb55c23e2ca4a012809b
Rafael Castro <castroguzmanrafa@outlook.com>
2023-04-29 09:03:12 -0600

    Formulario primera parte

--- P2_2021CG602_2020VV601_2021RR604/Controllers/casosReportadosController.cs
+++ P2_2021CG602_2020VV601_2021RR604/Controllers/casosReportadosController.cs
@@ -8,12 +8,18 @@ namespace P2_2021CG602_2020VV601_2021RR604.Controllers
     {
         private readonly ParcialBDContext _parcialBDContext;
 
-        public ParcialBDContext(ParcialBDContext parcialBDContext)
+        public casosReportadosController(ParcialBDContext parcialBDContext)
         {
             _parcialBDContext = parcialBDContext;
         }
         public IActionResult Index()
         {
+            var listaDeDepartamentos = (from m in _parcialBDContext.departamentos select m).ToList();
+            ViewData["listadoDeDepartamentos"] = new SelectList(listaDeDepartamentos, "idDepartamento", "nombreDepartamento");
+
+            var listaDeGeneros = (from m in _parcialBDContext.generos select m).ToList();
+            ViewData["listadoDeGeneros"] = new SelectList(listaDeGeneros, "idGenero", "nombreGenero");
+
             return View();
         }
     }
