da6fe197ca8de2c962ab2d0bf88d2634030cf701
Rafael Castro <castroguzmanrafa@outlook.com>
2023-04-29 07:51:54 -0600

    Entity Framework

--- P2_2021CG602_2020VV601_2021RR604/Models/casosReportados.cs
+++ P2_2021CG602_2020VV601_2021RR604/Models/casosReportados.cs
@@ -1,6 +1,11 @@
-﻿namespace P2_2021CG602_2020VV601_2021RR604.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P2_2021CG602_2020VV601_2021RR604.Models
 {
     public class casosReportados
     {
+    [Key]
+    public int idCasso { get; set; }
+     
     }
 }
