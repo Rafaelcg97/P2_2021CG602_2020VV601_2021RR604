153f8cf875422d2dc0e05b79159ec0d703fbf33b
castr <castr@DESKTOP-84D5GNI>
2023-04-29 07:27:28 -0600

    Add project files.

---
+++ P2_2021CG602_2020VV601_2021RR604/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P2_2021CG602_2020VV601_2021RR604.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
